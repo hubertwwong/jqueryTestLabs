cd776af0a13ea267790b01dfa802a8e900a29692
Hubert Wong <hubertwwong@gmail.com>
2013-06-15 17:35:28 -0700

    inital commit.

---
+++ app/learn.jquery.com/jqueryCore/basics/app.js
@@ -0,0 +1,12 @@
+/*
+ * jquery example
+ * 
+ * a really simple example that removes an element from the page.
+ * in this case, just an H1 element.
+ * 
+ * */
+
+// jquery dom ready function.
+$(function(){
+	$("h1").remove();
+});
