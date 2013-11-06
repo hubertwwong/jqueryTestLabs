730001303b0de0638e43d377bda62a7be6714eb8
Hubert Wong <hubertwwong@gmail.com>
2013-11-06 15:42:23 -0800

    update treehouse eample.

--- app/learn.jquery.com/plugin/treehouse/app.js
+++ app/learn.jquery.com/plugin/treehouse/app.js
@@ -9,6 +9,7 @@
  * */
 
 (function($) {
+	// name of the plugin is helloWorld.
     $.fn.helloWorld = function( options ) {
         // Establish our default settings
         var settings = $.extend({
@@ -20,19 +21,28 @@
         // complete takes a function call.
         // not sure how the security of this is.
 
-        return this.each( function() {
+		// good jquery plugins practice.
+		// use each. that way
+		// so if you have the .each, if the selector is for all h2 elements,
+		// this plugin will apply to all nodes.
+		//
+		// also note that since this is using the return this syntax.
+		// you are return jquery objects
+		// so you can still chain stuff...
+        return this.each(function() {
             $(this).text( settings.text );
 
-		    if ( settings.color ) {
-		        $(this).css( 'color', settings.color );
+		    if(settings.color) {
+		        $(this).css('color', settings.color);
 		    }
 		
-		    if ( settings.fontStyle ) {
-		        $(this).css( 'font-style', settings.fontStyle );
+		    if(settings.fontStyle) {
+		        $(this).css('font-style', settings.fontStyle);
 		    }
 		    
-		    if ( $.isFunction( settings.complete ) ) {
-        		settings.complete.call( this );
+		    // this is the callback
+		    if ($.isFunction(settings.complete)) {
+        		settings.complete.call(this);
     		}
         });
     };
@@ -44,7 +54,7 @@ $(function(){
 	$("#div_8").helloWorld({
 		text: 'OVERRIDE THE TEXT',
 		color: 'red'
-	});
+	}).addClass("foo");
 	
 	// call back function usage..
 	$("#div_9").helloWorld({
